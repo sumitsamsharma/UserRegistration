0e6851420550503eccb958e3a737436f4093e451
sumitsamsharma <sharmasumit94286@gmail.com>
2020-10-04 23:00:01 +0530

    Added first name option

---
+++ UserReg.java
@@ -0,0 +1,17 @@
+package user;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserReg 
+{
+	public static void main(String[] args)
+	{
+	    Scanner c=new Scanner(System.in);
+	    System.out.println("Enter email:");
+	    String n=c.nextLine();
+	    System.out.println("String is"+n);
+		System.out.println(Pattern.matches("^[A-Z]{1}[a-z0-9]{2,}$",n));
+		c.close();
+	}  
+}
