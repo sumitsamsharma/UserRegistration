6cb32d999231ac4f9e72ce8cb1e3561bd7763397
sumitsamsharma <sharmasumit94286@gmail.com>
2020-10-04 23:08:57 +0530

    Added Last name option

--- UserReg.java
+++ UserReg.java
@@ -11,7 +11,7 @@ public class UserReg
 	    System.out.println("Enter email:");
 	    String n=c.nextLine();
 	    System.out.println("String is"+n);
-		System.out.println(Pattern.matches("^[A-Z]{1}[a-z0-9]{2,}$",n));
+		System.out.println(Pattern.matches("^[A-Z]{1}[a-z0-9]{2,}\s[A-Z]{1}[a-z0-9]{2,}$",n));
 		c.close();
 	}  
 }
