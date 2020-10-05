ddd7c8546649a9e413c36f5501bc7010b07e8d66
sumitsamsharma <sharmasumit94286@gmail.com>
2020-10-05 09:14:41 +0530

    added should clear all sample emails

--- UserReg.java
+++ UserReg.java
@@ -30,14 +30,14 @@ public class UserReg
 	{
 		System.out.println("Enter email");
 		String email = scanner.nextLine();
-		if (Pattern.matches("^[_A-Za-z0-9-+&*-]+(\\.[_A-Za-z0-9-]+)*@"+"[A-Za-z0-9-+&*-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2})$",email))
+		if (Pattern.matches("^[_A-Za-z0-9-+&*-]+(\\.[_A-Za-z0-9-]+)*@"+"[A-Za-z0-9-+&*-]+(\\.[A-Za-z0-9]+)(\\.[A-Za-z]{2})*$",email))
 			System.out.println("it is valid e-mail");
 		else
 			System.out.println("it is not valid email");		
 	}
 	
 	public static void mobile() {
-		System.out.println("Enter Mobile Number With Country Code(space between code and number)");
+		System.out.println("Enter mobile no with a country code(give a space between code and no.)");
 		String mobile = scanner.nextLine();
 		if (Pattern.matches("^[1-9]{2}\s{1}[1-9]{1}[0-9]{9}$",mobile))
 			System.out.println("Valid Mobile no."+mobile);
