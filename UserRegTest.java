3831a4f4cd48e43aa46f32bf2796430cd68e9d70
sumitsamsharma <43275389+sumitsamsharma@users.noreply.github.com>
2020-10-07 08:36:23 +0530

    Update UserRegTest.java

--- UserRegTest.java
+++ UserRegTest.java
@@ -1,41 +1,43 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.Collection;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class UserRegTest {
+    private UserReg obj1;
+    private String mail;
+    private boolean exp_value;
 
-    @Test
-    public void testFirstName()
-    {
-        assertTrue(UserReg.first("Sharma"));
+    @Before
+    public void initialize() {
+        obj1=new UserReg();
+    }
+
+    public UserRegTest(String mail, boolean exp_value) {
+        this.mail=mail;
+        this.exp_value=exp_value;
+    }
+    @Parameterized.Parameters
+    public static Collection testEmails(){
+        return Arrays.asList(new Object[][] { {"abc@gmail.com",true},{"abc-100@gmail.com",true},
+                {"abc21@abc.com",true},{"abc.185@gmail.com.au",true},{"abc@1.com",true},
+                {"abc256@gmail.a",false},{"abc123@.com",false},{"abc123@.com.com",false},
+                {"abc..@abc.com",false},{"abc*@abc.com",false},{".abc@*$.com",false},{"abc.!1999@ebc.com",false},
+                {"abc.@abc.com",false},{"abc@abc@abc.com",false},{".abc@abc.com",false},{"abc@abc.com.1a",false},
+                {"abc@qwe.com.ag.aq",false}});
     }
 
     @Test
-    public void testLastName()
-    {
-        assertTrue(UserReg.last("Sharma"));
+    public void givenEmail_whenEntered_shouldReturnValidation() {
+        System.out.println("Parameterised Emails are "+mail);
+        assertEquals(exp_value,UserReg.email(mail));
     }
 
-    @Test
-    public void testEmail()
-    {
-        assertTrue(UserReg.email("Sharma@gmail.com"));
-    }
-
-    @Test
-    public void testMobile()
-    {
-        assertTrue(UserReg.mobile("91 8123614499"));
-    }
-
-    @Test
-    public void testPassword()
-    {
-        assertTrue(UserReg.password("Qwerty123!"));
-    }
-
-    public void givenString_whenEntered_shouldReturnSad() {
-        assertEquals("sad",UserReg.isHappy("Happy and not sad"));
-    }
 
 }
