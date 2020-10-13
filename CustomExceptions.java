5fe51c1b43562b230eaaa2fa8c97eeb67567b871
sumitsamsharma <sharmasumit94286@gmail.com>
2020-10-13 10:27:50 +0530

    Added lambda expressions

---
+++ CustomExceptions.java
@@ -0,0 +1,10 @@
+package user;
+
+public class CustomExceptions extends Exception
+{
+    public CustomExceptions(String s)
+    {
+        super(s);
+    }
+}
+
--- UserReg.java
+++ UserReg.java
@@ -1,10 +1,10 @@
 package user;
 
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class UserReg 
+public class UserReg
 {
+<<<<<<< HEAD
 	static Scanner scanner = new Scanner(System.in);
 
 	public static void first() 
@@ -61,4 +61,58 @@ public class UserReg
 		password();
 	}
 }
+=======
+    static boolean happy=false;
+    public static boolean first (String f_name) throws CustomExceptions {
+        if(Pattern.matches("[A-Z]{1}[a-z]{2,}", f_name))
+        {
+            return true;
+        }
+        else
+            {
+            throw new CustomExceptions("First name unexpected");
+        }
+    }
 
+    public static boolean last(String l_name) throws CustomExceptions
+    {
+        if(Pattern.matches("^[A-Z]{1}[a-z]{2,}$",l_name))
+            return true;
+        else
+            throw new CustomExceptions("Last name unexpected");
+
+    }
+
+    public static boolean email(String mail) throws CustomExceptions
+    {
+        if(Pattern.matches("^[_A-Za-z0-9-+&*-]+(\\.[_A-Za-z0-9-]+)*@"+"[A-Za-z0-9-+&*-]+(\\.[A-Za-z0-9]+)(\\.[A-Za-z]{2})*$",mail))
+            return true;
+        else
+            throw new CustomExceptions("Email unexpected");
+    }
+
+    public static boolean mobile(String phone) throws CustomExceptions{
+        if(Pattern.matches("[0-9]{2}\\s{1}[1-9]{1}[0-9]{9}",phone))
+            return true;
+        else
+            throw new CustomExceptions("Mobile number unexpected");
+>>>>>>> UC12_custom_exceptions
+
+    }
+
+    public static boolean password(String pass) throws CustomExceptions {
+
+        if(Pattern.matches("^(?=.*\\d)"+"(?=.*[a-z])(?=.*[A-Z])"+"(?=.*[#@%$^=+&])"+"(?=\\S+$).{8,}$",pass))
+            return true;
+        else
+            throw new CustomExceptions("Password unexpected");
+    }
+
+    public static String isHappy(String message)
+    {
+        if(message.contains("Sad"))
+            return "Sad";
+        else
+            return "Happy";
+    }
+}
